a464e361ce797879d2a1c25a1f39a063291288ad
Sean Mansell <thearknano@gmail.com>
2017-05-05 15:20:25 +0930

    Started ui prototyping

---
+++ Assets/MoveMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DentedPixel;
+
+public class MoveMenu : MonoBehaviour {
+
+    public float moveTime;
+
+    public Vector3 moveInPos;
+    public Vector3 moveOutPos;
+
+    RectTransform rect;
+
+	// Use this for initialization
+	void Start () {
+        rect = gameObject.GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	public void MoveIn () {
+        LeanTween.move(rect, moveInPos, moveTime).setEase(LeanTweenType.easeInOutQuad);
+    }
+
+    	public void MoveOut()
+    {
+        LeanTween.move(rect, moveOutPos, moveTime).setEase(LeanTweenType.easeInOutQuad);
+    }
+}
